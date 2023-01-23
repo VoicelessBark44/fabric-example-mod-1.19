95ec30268ec381e4d601b0b60a7b8c8d0e881100
VoicelessBark44 <34280572+VoicelessBark44@users.noreply.github.com>
2023-01-22 16:54:58 -0800

    Initial commit

---
+++ src/main/java/net/fabricmc/stinga/Main.java
@@ -0,0 +1,37 @@
+package net.fabricmc.stinga;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+
+	//public static final Item STINGA_SWORD = new Item(new FabricItemSettings());
+
+
+	public static final Item STINGA_SWORD =
+			Registry.register(Registries.ITEM, new Identifier("stingasword", "stinga_sword"),
+					new Item(new FabricItemSettings()));
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("stingasword");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		//Registry.register(Registries.ITEM, new Identifier("stingasword", "stinga_sword"), STINGA_SWORD);
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
